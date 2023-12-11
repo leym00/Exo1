7213f1fddb80a616870868be1a5bf72ea79732a7
DESKTOP-7D6MIQ0\Florine Dobigny <florine.dobigny@gmail.com>
2023-12-11 11:14:21 +0100

    Add project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int monstres = 4;
+
+            Console.WriteLine("Hello World ! " + monstres);
+            Console.ReadLine();
+        }
+    }
+}
